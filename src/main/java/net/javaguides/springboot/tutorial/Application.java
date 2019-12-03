9da7e6e15f96636a5fd1085aff537e025c714bc1
ricardo.carreira <ricardo.carreira.613@gmail.com>
2019-12-03 10:18:33 +0000

    Add .gitignore Update App to work with websphere liberty

--- src/main/java/net/javaguides/springboot/tutorial/Application.java
+++ src/main/java/net/javaguides/springboot/tutorial/Application.java
@@ -3,11 +3,38 @@ package net.javaguides.springboot.tutorial;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.support.ErrorPageFilter;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application {
-
+public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	@Bean
+	public ErrorPageFilter errorPageFilter() {
+		return new ErrorPageFilter();
+	}
+	@Bean
+	public FilterRegistrationBean disableSpringBootErrorFilter(ErrorPageFilter filter) {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(filter);
+		filterRegistrationBean.setEnabled(false);
+		return filterRegistrationBean;
+	}
+
+	/* Original Code
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	*/
 }
